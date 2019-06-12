c4eccfaf65b0244ce5295304f3d7dd646ef8a560
Diego Guisande <DGuisande873@gmail.com>
2019-06-11 17:12:55 -0700

    final edit with zoom sliders

--- JS/Mbrot.js
+++ JS/Mbrot.js
@@ -1,30 +1,51 @@
+var minval = -0.5;
+var maxval = 0.5;
+
+var minSlider; 
+var maxSlider;
+
 function setup(){
-    createCanvas(360,360)
+    createCanvas(200,200)
     pixelDensity(1);
+
+    //determines how the zoom run in the browser 
+    minSlider = createSlider(-2.5, 0, -2.5, 0.01);
+    maxSlider = createSlider(0, 2.5, 2.5, 0.01);
+}
+
+function draw(){
+    var maxiterations = 100; 
+
     loadPixels(); 
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-
-            var a = map(x,0,width,-2,2);
-            var b = map(y,0,height,-2,2); 
+            
+            //zoom dependency 
+            var a = map(x,0,width,minSlider.value(),maxSlider.value());
+            var b = map(y,0,height,minSlider.value(),maxSlider.value()); 
 
             var ca = a;
             var cb = b; 
             var n = 0; 
             
 
-            while (n < 100){
+            while (n < maxiterations){
                 var aa = a * a - b * b;
-                var bb = 2 * a * b; 
+                var bb = 2 * a * b;  
                 a = aa + ca;
                 b = bb + cb; 
-                if (a + b > 16) {
+                if (abs(a + b) > 16) {
                     break;
                 }
                 n++; 
             }
-
-            var bright = map(n,0,100,0,255);
+            
+            //this is the section that will determine the coloring pattern of the Mandelbrot Set
+            var bright = map(n,0,maxiterations,0,1);
+            bright = map(sqrt(bright),0,1,0,255); 
+            if(n === maxiterations){
+                bright = 0; 
+            }
 
             var pix = (x + y * width) * 4; 
             pixels[pix + 0] = bright;
@@ -35,8 +56,4 @@ function setup(){
         }   
     }
     updatePixels(); 
-}
-
-function draw(){
-
 }
